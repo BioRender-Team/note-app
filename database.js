48e67192535d9ff9b0cf9120010bc7e5720bbb59
Katya <ekaterina.shteyn@gmail.com>
2020-08-10 15:12:19 -0400

    frontend only test

--- database.js
+++ database.js
@@ -4,26 +4,64 @@ import * as sqlite from 'sqlite3';
 const db = new sqlite.Database(':memory:');
 
 db.serialize(function () {
-  db.run('CREATE TABLE notes (id INTEGER PRIMARY KEY AUTOINCREMENT, value TEXT)');
-  const stmt = db.prepare('INSERT INTO notes VALUES (?,?)');
-  for (var i = 0; i < 3; i++) {
-    stmt.run(i, `Sample Note ${i}`);
-  }
-  stmt.finalize();
+    db.run('CREATE TABLE notes (id INTEGER PRIMARY KEY AUTOINCREMENT, value TEXT)');
+    const stmt = db.prepare('INSERT INTO notes VALUES (?,?)');
+    for (var i = 0; i < 3; i++) {
+        stmt.run(i, `Sample Note ${i}`);
+    }
+    stmt.finalize();
 });
 
-export function readNotes() {
-  return new Promise(r => {
-    db.all('SELECT * FROM notes', (err, result) => r(result));
-  });
+export async function readNotes() {
+    return await new Promise(resolve => {
+        db.all('SELECT * FROM notes', (err, result) => resolve(result));
+    });
+}
+
+const insertNote = async () => {
+    return await new Promise(resolve => {
+        db.run(`INSERT INTO notes VALUES (null, '${message}')`, function () {
+            db.all(`SELECT * FROM notes WHERE id = ${this.lastID}`, (err, result) => {
+                if (result && result.length === 1) {
+                    resolve(result[0])
+                }
+            });
+        });
+    });
 }
 
 export async function writeNote(message) {
-  return await db.run(`INSERT INTO notes VALUES (null, '${message}')`);
+    return await new Promise(resolve => {
+        db.run(`INSERT INTO notes VALUES (null, '${message}')`, function () {
+            db.all(`SELECT * FROM notes WHERE id = ${this.lastID}`, (err, result) => {
+                if (result && result.length === 1) {
+                    resolve(result[0])
+                }
+            });
+        });
+    });
 }
 
+export async function updateNote(id, message) {
+    return await new Promise(resolve => {
+        db.run(`UPDATE notes SET value = '${message}' WHERE id = ${id}`, function () {
+            db.all(`SELECT * FROM notes WHERE id = ${id}`, (err, result) => {
+                if (result && result.length === 1) {
+                    resolve(result[0])
+                }
+            });
+        });
+    });}
 
-export function deleteNote(message) {
-  db.run(`DELETE FROM notes WHERE value = '${message}'`);
+export async function deleteNote(id) {
+    return await new Promise(resolve => {
+        db.run(`DELETE FROM notes WHERE id = '${id}'`, (err, result) => {
+            if (err) {
+                reject();
+            }
+            resolve()
+        });
+    });
+
 }
 
--- server.js
+++ server.js
@@ -1,19 +1,51 @@
 import express from 'express';
-import {deleteNote, readNotes, writeNote} from "./database";
+import {deleteNote, readNotes, writeNote, updateNote} from "./database";
 
 const app = express();
 app.use(express.json());
 
+// Returns all note objects
 app.get('/api/notes', async (req, res, next) => {
-  res.send(await readNotes());
+    try {
+        const notes = await readNotes()
+        res.status(200).send(notes)
+    } catch (err) {
+        res.status(400).send()
+    }
 });
 
+// Adds the note and sends back a new note object
+// req.body.note: string
 app.post('/api/note', async (req, res, next) => {
-  res.send(await writeNote(req.body.note));
+    try {
+        const newNote = await writeNote(req.body.note)
+        res.status(200).send(newNote)
+    } catch (err) {
+        res.status(400).send()
+    }
 });
 
-app.delete('/api/note', async (req, res, next) => {
-  res.send(await deleteNote(req.body.note));
+// Updates the note with id to new value and sends back the new note object
+// req.params.id: integer
+// req.body.note: string
+app.put('/api/note/:id', async (req, res, next) => {
+    try {
+        const newNote = await updateNote(req.params.id, req.body.note)
+        res.status(200).send(newNote)
+    } catch (err) {
+        res.status(400).send()
+    }
+});
+
+// Deletes note by id and returns a status code 200 on success
+// req.params.id: integer
+app.delete('/api/note/:id', async (req, res, next) => {
+  try {
+      await deleteNote(req.params.id)
+      res.status(200).send()
+  } catch (err) {
+    res.status(400).send()
+  }
 });
 
 app.use('*', express.static(__dirname + '/public'));
