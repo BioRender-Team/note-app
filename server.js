e77a21e4386de785c87b585b7df754ab11c84826
Katya <ekaterina.shteyn@gmail.com>
2020-10-22 23:07:05 -0400

    backend only version of the test

--- server.js
+++ server.js
@@ -1,51 +1,27 @@
 import express from 'express';
-import {deleteNote, readNotes, writeNote, updateNote} from "./database";
+import {readNotes} from "./database";
 
 const app = express();
 app.use(express.json());
 
 // Returns all note objects
 app.get('/api/notes', async (req, res, next) => {
-    try {
-        const notes = await readNotes()
-        res.status(200).send(notes)
-    } catch (err) {
-        res.status(400).send()
-    }
+    const notes = await readNotes()
+    res.status(200).send(notes)
 });
 
 // Adds the note and sends back a new note object
 // req.body.note: string
 app.post('/api/note', async (req, res, next) => {
-    try {
-        const newNote = await writeNote(req.body.note)
-        res.status(200).send(newNote)
-    } catch (err) {
-        res.status(400).send()
-    }
-});
-
-// Updates the note with id to new value and sends back the new note object
-// req.params.id: integer
-// req.body.note: string
-app.put('/api/note/:id', async (req, res, next) => {
-    try {
-        const newNote = await updateNote(req.params.id, req.body.note)
-        res.status(200).send(newNote)
-    } catch (err) {
-        res.status(400).send()
-    }
+    // todo: write to db
+    res.status(200).send({id: Math.random()*100, value: req.body.note})
 });
 
 // Deletes note by id and returns a status code 200 on success
-// req.params.id: integer
-app.delete('/api/note/:id', async (req, res, next) => {
-  try {
-      await deleteNote(req.params.id)
-      res.status(200).send()
-  } catch (err) {
-    res.status(400).send()
-  }
+// req.body.note: string
+app.delete('/api/note', async (req, res, next) => {
+    // todo: delete the note
+    res.status(200).send()
 });
 
 app.use('*', express.static(__dirname + '/public'));
