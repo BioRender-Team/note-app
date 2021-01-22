7b43c2d7a547bb36fa726605fe99d4ee797b8219
BioCarmen <carmen@biorender.com>
2021-01-22 12:32:38 -0500

    set error message

--- server.js
+++ server.js
@@ -4,14 +4,28 @@ import { deleteNote, readNotes, resetDB, writeNote } from "./database";
 const app = express();
 app.use(express.json());
 
+// Returns all note objects
 app.get("/api/notes", async (req, res, next) => {
-  res.send(await readNotes());
+  try {
+    const notes = await readNotes();
+    res.status(200).send(notes);
+  } catch (err) {
+    res.status(400).send();
+  }
 });
-
+// Adds the note and sends back a new note object
+// req.body.note: string
 app.post("/api/note", async (req, res, next) => {
-  res.send(await writeNote(req.body.note));
+  try {
+    const newNote = await writeNote(req.body.note);
+    res.status(200).send(newNote);
+  } catch (err) {
+    res.status(400).send();
+  }
 });
 
+// Deletes note by id and returns a status code 200 on success
+// req.params.id: integer
 app.delete("/api/note/:id", async (req, res, next) => {
   try {
     await deleteNote(req.params.id);
